4384155a76f35d6bde4b535416d55a2d345d3705
Kevin Membreve <kevinmembreve32@gmail.com>
2023-04-27 21:24:33 +0800

    fix messages data

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -6,7 +6,7 @@ export const createConversation = async (req, res, next) =>{
     const newConversation = new Conversation({
         id: req.isSeller ? req.userId + req.body.to : req.body.to + req.userId,
         sellerId: req.isSeller ? req.userId : req.body.to,
-        buyerId: req.isSeller ? req.userId : req.body.to,
+        buyerId: req.isSeller ? req.body.to : req.userId,
         readBySeller: req.isSeller,
         readByBuyer: !req.isSeller,
     });
